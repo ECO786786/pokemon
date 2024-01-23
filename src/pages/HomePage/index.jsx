6025abba2d81c6c1cb69dfe08932c8812a993ad3
Emma Collins <lowpee56@hotmail.com>
2024-01-23 00:12:46 +0000

    Add Pokémon logo image and apply styling to homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,14 @@
+import pokemonLogo from "./img/pokemon-logo-pokemon-icon-transparent-free-png.webp";
+
 const HomePage = () => {
   return (
     <main>
-      <h1>Pokemon!</h1>
-      <p>Welcome to the World of Pokemon!</p>
+      <h1>Welcome to the world of</h1>
+      <img src={pokemonLogo} alt="Pokemon Logo" />
+      <p>
+        Discover a world of creatures, battles, and legendary adventures in
+        Pokémon.
+      </p>
     </main>
   );
 };
